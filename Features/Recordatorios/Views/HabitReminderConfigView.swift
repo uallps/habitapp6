af75e80156ff3e20e9b92e5f30b6c85050b94ddb
Martinez Gutierrez Raul <rmg429@inlumine.ual.es>
2025-12-24 14:47:35 +0100

    Crea archivos para definir la estructura de mejor manera

---
+++ Features/Recordatorios/Views/HabitReminderConfigView.swift
@@ -0,0 +1,11 @@
+/// Este archivo esta pensado para construir un
+/// componente de vista donde se incluya un panel
+/// en el que se configura la hora en la que se
+/// crea el recordatorio para ese habito en concreto.
+
+/// Debería aparecer al momento de crear un hábito
+/// o de editarlo, y se debe configurar una hora.
+/// Si el hábito es diario, se crea el recordatorio
+/// todos los días a la hora definida. Si es semanal,
+/// el hábito se recuerda cada dos días a la hora
+/// hasta que se de por hecho.
